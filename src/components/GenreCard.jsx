2259c6748a8e1dd270db5652665f2b1cb1ca68cb
Enzo Miletta <161778417+Enzo-Miletta@users.noreply.github.com>
2024-11-08 22:14:41 -0300

    First commit just for save the project

---
+++ src/components/GenreCard.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function GenreCard({name}) {
+    return(
+        <Link className="w-auto" to={`/genre/${name}`}>
+            <div className='bg-green-700 w-64 h-28 shadow-md rounded-md p-4 relative'>
+                <h3 className='text-lg text-white font-bold absolute bottom-4 right-4'>{name}</h3>
+            </div>
+        </Link>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+// import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import BotaoLogin from "./BotaoLogin";
+import { useState } from "react";
+
+export default function Header(){
+
+    const [isLogged, setIsLogged] = useState(false);
+
+    const handleLogin = () => {
+        setIsLogged(!isLogged);
+    }
+    
+    return(
+        <>
+            <header className="w-full h-10 flex justify-between items-center p-10 mb-10 bg-green-700">
+                <div>
+                  
+                    <h1>PopCorn Studios</h1>
+                </div>
+                <nav>
+                    <ul className="flex gap-5">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/movies">Filmes</NavLink></li>
+                        <li><NavLink to="/genre">Gêneros</NavLink></li>
+                        <li><NavLink to="/favoritos">Favoritos</NavLink></li>
+                        <li><NavLink to="/contato">Contato</NavLink></li>
+                        {isLogged && <li><NavLink to="/settings">Configurações</NavLink></li>}
+                    </ul>
+                </nav>
+                <BotaoLogin isLogged={isLogged} handleLogin={handleLogin}/>
+            </header>
+        </>
+    )
+}
